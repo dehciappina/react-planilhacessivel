87155d2bdb3d9b9e47e8b4bfa8493bb1164e01f2
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2019-06-10 23:06:57 -0300

    Add files via upload

---
+++ precache-manifest.63076f18b501f57b6ede43350e733bdf.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad163702cd20558123962b290991329",
+    "url": "/react-planilhacessivel/index.html"
+  },
+  {
+    "revision": "61d3d4b806943a1a9ca9",
+    "url": "/react-planilhacessivel/static/css/main.308a0676.chunk.css"
+  },
+  {
+    "revision": "f3a0a10109a5859e6c45",
+    "url": "/react-planilhacessivel/static/js/2.c56be15f.chunk.js"
+  },
+  {
+    "revision": "61d3d4b806943a1a9ca9",
+    "url": "/react-planilhacessivel/static/js/main.68cdc3e2.chunk.js"
+  },
+  {
+    "revision": "a9b439199110363d305a",
+    "url": "/react-planilhacessivel/static/js/runtime~main.f689e0fb.js"
+  },
+  {
+    "revision": "173ae569bc7eb702c599a0676f52986f",
+    "url": "/react-planilhacessivel/static/media/VERSÃO GRATUITA - Calculadora Financeira Analítica.173ae569.xlsm"
+  },
+  {
+    "revision": "74b7916470a026e6e0cda122bd38c540",
+    "url": "/react-planilhacessivel/static/media/VERSÃO GRATUITA - Planilha de Análise de Operações de Consórcio.74b79164.xlsm"
+  },
+  {
+    "revision": "d205a7cb256a1df300b27d8e668f199c",
+    "url": "/react-planilhacessivel/static/media/VERSÃO GRATUITA - Planilha de Análise de Operações de Empréstimo.d205a7cb.xlsm"
+  },
+  {
+    "revision": "53da66b6842c66de6fdf407804f491b6",
+    "url": "/react-planilhacessivel/static/media/VERSÃO GRATUITA - Planilha de Cálculo de Antecipação de Recebíveis.53da66b6.xlsm"
+  },
+  {
+    "revision": "3eae337df6486d13f1a3f11e4f92801c",
+    "url": "/react-planilhacessivel/static/media/VERSÃO GRATUITA - Programa Emissor de Nota de Debito.3eae337d.xlsm"
+  },
+  {
+    "revision": "a38723fe1819077c0343b8cfdac59195",
+    "url": "/react-planilhacessivel/static/media/VERSÃO GRATUITA - Programa Emissor de RPA e TAC.a38723fe.xlsm"
+  },
+  {
+    "revision": "cdaa97f25ea1a35dd061112459d8ce20",
+    "url": "/react-planilhacessivel/static/media/VERSÃO GRATUITA - Programa de Controle Financeiro.cdaa97f2.xlsm"
+  },
+  {
+    "revision": "dbedd35fbd2f0fa9456146069837b4b6",
+    "url": "/react-planilhacessivel/static/media/analise_emprestimo.dbedd35f.svg"
+  },
+  {
+    "revision": "80fb42808e8490e1951ddaba83d09f79",
+    "url": "/react-planilhacessivel/static/media/bg1.80fb4280.jpg"
+  },
+  {
+    "revision": "dea2a2c1fd199898b3e862cee0f9de22",
+    "url": "/react-planilhacessivel/static/media/bg2.dea2a2c1.jpg"
+  },
+  {
+    "revision": "3dd6061e14f7c5d141a4d908520993e6",
+    "url": "/react-planilhacessivel/static/media/bg3.3dd6061e.jpg"
+  },
+  {
+    "revision": "62efc86fb86c70ca4acacf17a0c626d1",
+    "url": "/react-planilhacessivel/static/media/bg4.62efc86f.jpg"
+  },
+  {
+    "revision": "5c6eea446b84beee05e3ee221f4024a6",
+    "url": "/react-planilhacessivel/static/media/calculadora.5c6eea44.svg"
+  },
+  {
+    "revision": "4af72468b2d6bbab664939df616eb55e",
+    "url": "/react-planilhacessivel/static/media/consorcio.4af72468.svg"
+  },
+  {
+    "revision": "bbeaac54809858ffe150c2cc8859465f",
+    "url": "/react-planilhacessivel/static/media/controle_financeiro.bbeaac54.svg"
+  },
+  {
+    "revision": "389937dfe5b198f8a7ef3325d4247966",
+    "url": "/react-planilhacessivel/static/media/debito.389937df.svg"
+  },
+  {
+    "revision": "2444fb7f87378655796632b6f6d4a4f2",
+    "url": "/react-planilhacessivel/static/media/download_bt.2444fb7f.svg"
+  },
+  {
+    "revision": "21fec864924b84729ba06ed375e3da97",
+    "url": "/react-planilhacessivel/static/media/feature1.21fec864.svg"
+  },
+  {
+    "revision": "cdf78655b7d34e95f60efe8ef4cb85dd",
+    "url": "/react-planilhacessivel/static/media/feature2.cdf78655.svg"
+  },
+  {
+    "revision": "6217f856927f957be04194df44c26656",
+    "url": "/react-planilhacessivel/static/media/feature3.6217f856.svg"
+  },
+  {
+    "revision": "9ca9cc964aa3061ed9eed96f5c6f927f",
+    "url": "/react-planilhacessivel/static/media/fluxo_financeiro.9ca9cc96.svg"
+  },
+  {
+    "revision": "6fab173109a22e7f0443bb878d6b8c44",
+    "url": "/react-planilhacessivel/static/media/impostos_imobiliarios.6fab1731.svg"
+  },
+  {
+    "revision": "bee03bb710260303867c6a8dee01e43c",
+    "url": "/react-planilhacessivel/static/media/indices.bee03bb7.svg"
+  },
+  {
+    "revision": "98e96e2a162f314bf18f7905c9e85eef",
+    "url": "/react-planilhacessivel/static/media/ms.98e96e2a.svg"
+  },
+  {
+    "revision": "b6c86b5c3d86963b7257dd16e3e10e1b",
+    "url": "/react-planilhacessivel/static/media/real_presumido.b6c86b5c.svg"
+  },
+  {
+    "revision": "0eff3f861d9d0e99d5d71cd068f379ed",
+    "url": "/react-planilhacessivel/static/media/recebiveis.0eff3f86.svg"
+  },
+  {
+    "revision": "d32471f64f856905c28f0292de73292a",
+    "url": "/react-planilhacessivel/static/media/recibo.d32471f6.svg"
+  },
+  {
+    "revision": "40ff69179b00139c2ad082e3ac72a294",
+    "url": "/react-planilhacessivel/static/media/rpa.40ff6917.svg"
+  },
+  {
+    "revision": "c5d571b7ace697423dc902dee4a02d8b",
+    "url": "/react-planilhacessivel/static/media/simples_real_presumido.c5d571b7.svg"
+  },
+  {
+    "revision": "96f890d77c392d618eb32b8057b6ca1d",
+    "url": "/react-planilhacessivel/static/media/udemy_curso1.96f890d7.JPG"
+  },
+  {
+    "revision": "5e3f2a354e9ff4ab5a95886856ced35e",
+    "url": "/react-planilhacessivel/static/media/udemy_curso1_mobile1.5e3f2a35.JPG"
+  },
+  {
+    "revision": "6f93a4dd3876e0a1f28781d4c233b4e1",
+    "url": "/react-planilhacessivel/static/media/udemy_curso1_mobile2.6f93a4dd.JPG"
+  },
+  {
+    "revision": "e25165fb2e6aae51520312825721575d",
+    "url": "/react-planilhacessivel/static/media/whatsapp_logo.e25165fb.svg"
+  }
+]);
